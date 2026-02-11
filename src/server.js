f954ee09e38efbe24596758a1209ea4cc2b03908
Mohammed Waleed <mwse00022@gmail.com>
2026-02-11 20:05:41 +0200

    Refactor import statements in server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
-import 'dotenv/config' // 👈 السطر ده لازم يبقى أول واحد كدة عشان يحل المشكلة
+import 'dotenv/config' 
 import mongoose from 'mongoose'
-import { app } from './app.js'
+import { app } from './app.js' 
 
-// الاتصال بقاعدة البيانات
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err))
 
-// تشغيل السيرفر
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`)
 })
