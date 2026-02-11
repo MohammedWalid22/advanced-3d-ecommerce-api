955425cefb9044937047b332e87885b20b551d91
Mohammed Waleed <mwse00022@gmail.com>
2026-02-11 20:07:09 +0200

    Refactor import statement for reviewRouter

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,17 +3,17 @@ import { getProducts, getProductById, createProduct, updateProduct, deleteProduc
 import upload from '../middlewares/upload.js'
 import { protect } from '../middlewares/auth.js'
 import { adminOnly } from '../middlewares/admin.js'
-import reviewRouter from './reviews.js' // 👈 1. استيراد ملف الريفيوهات
+import reviewRouter from './reviews.js' 
 
 const router = express.Router()
 
-// 👈 2. أي طلب يروح للينك ده، حوله فوراً لملف reviews.js
+ 
 router.use('/:productId/reviews', reviewRouter)
 
 router.get('/', getProducts)
 router.get('/:id', getProductById)
 
-// حماية الراوتس اللي جاية (للأدمن فقط)
+
 router.use(protect, adminOnly)
 
 router.post('/', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'model3D', maxCount: 1 }]), createProduct)
